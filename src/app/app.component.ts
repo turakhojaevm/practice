ee93cdb6d4f0e323c264e9fe4f0050050fdb9c5f
Makhkamkhoja <m.turakhojaev@eskhata.tj>
2025-10-02 14:29:45 +0500

    feat: 0001 first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    DatePipe
+  ],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'practice';
+  value = 0;
+  updatedAt = new Date();
+
+  get disabled(): boolean {
+    return this.value <= 0;
+  }
+
+  increment(): void {
+    this.value++;
+    this.updatedAt = new Date();
+  }
+
+  decrement(): void {
+    this.value--;
+    this.updatedAt = new Date();
+  }
+
+  reset(): void {
+    this.value = 0;
+    this.updatedAt = new Date();
+  }
 }
