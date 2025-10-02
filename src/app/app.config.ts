d52401fb920e151db4fc07cb38e81ba467be35a3
Makhkamkhoja <m.turakhojaev@eskhata.tj>
2025-10-02 15:09:21 +0500

    feat: 0002 second commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideStore} from '@ngrx/store';
+import {reducers, metaReducers} from './reducers';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideStore(reducers, {metaReducers}),
+    provideStoreDevtools({maxAge: 25, logOnly: false})
+  ],
 };
